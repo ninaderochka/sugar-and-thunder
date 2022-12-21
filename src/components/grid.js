adc7994f02fd366e758369f7008add40f8157555
Nina Hawari <nina.hawari@gmail.com>
2022-12-21 23:48:21 +0200

    updating grid

--- src/components/grid.js
+++ src/components/grid.js
@@ -4,8 +4,6 @@ const Grid = (props) => (
     <div className="flex items-center justify-center">
 <div className="grid grid-cols-3 gap-2 p-5">
         <div className="bg-white border-border-grey shadow-shadow-grey/25 font-poppins font-normal text-center box-border border-2 rounded-md w-box-width h-box-height absolute">{props.children}</div>
-        <div className="bg-white border-border-grey shadow-shadow-grey/25 font-poppins font-normal text-center box-border border-2 rounded-md w-box-width h-box-height absolute">{props.children}</div>
-        <div className="bg-white border-border-grey shadow-shadow-grey/25 font-poppins font-normal text-center box-border border-2 rounded-md w-box-width h-box-height absolute">{props.children}</div>
     </div>
     </div>
 )
