dcf2ba66649816a0511807f40f954a9e401cfbc4
Zeynab Hamad <zaynabhamad950@gmail.com>
2022-12-26 00:08:58 +0300

    finally compleated

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,18 +6,18 @@ export default function Header() {
   return (
 
     <div className="text-black font-normal " >
-      <div className="absolute top-[126px] left-0 w-full h-full bg-cover" ><img alt="#" src={background}/></div>
+       <img className=" absolute bg-center imagee" alt="#" src={background}/>
     
-      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col absolute">
+      <div className="container flex py-24 md:flex-row flex-col absolute">
 
-        <div className="font-Poppins lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left md:mb-0 items-center mt-">
+        <div className="font-Poppins lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left md:mb-0 items-center">
           
           <p className="text-black ml-36 text-3xl sm:text-3xl">WE ARE HERE TO</p>
           <p className="text-black ml-36 text-9xl ">HELP</p>         
 
           {/* button */}
           <div className=" ml-32 py-5 mt-12">
-            <button type="button" className="text-2xl rounded bg-cyan-300 py-2 px-3">BOOK AN APPOINTMENT</button>
+            <button type="button" className="text-2xl rounded bg-cyan-300 py-2 px-3 focus:outline-none">BOOK AN APPOINTMENT</button>
           </div>
 
          </div>
