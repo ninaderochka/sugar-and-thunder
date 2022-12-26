d5c32ba0a1b6f3e7581c1a8066f897bcfd8aed73
marshall <mhamad.marshall@gmail.com>
2022-12-26 13:04:14 +0300

    Navbar Updated

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './Button.css';
 
-export function Button() {
+const Button = (props) => {
   return (
-    <Link to="sign-up">
-      <button className="btn">Sign Up</button>
-    </Link>
+    <button
+      className="bg-indigo-600 text-white font-[Poppins] py-2 px-6 rounded md:ml-8 hover:bg-indigo-400
+    duration-500"
+    >
+      {props.children}
+    </button>
   );
-}
+};
+
+export default Button;
--- src/components/pages/Store.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Store() {
-  return (
-    <>
-      <h1 className="store">Store</h1>
-    </>
-  );
-}
