77c7921b5753af5f9cde70afdf4c9d398ef402fe
Nina Hawari <nina.hawari@gmail.com>
2022-12-26 23:22:35 +0200

    changed empty a tags in footer to buttons for now to compile without errors

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
   return (
     <div className="App">
       <Communicate />
-       <RecentBlog />
+      <RecentBlog />
       <Tickets />
-       <Footer />
+      <Footer />
     </div>
   );
 }
