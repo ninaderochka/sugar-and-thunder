9d26f527257614eef9efdfe895c290c01798dc67
Nina Hawari <nina.hawari@gmail.com>
2022-12-26 21:53:38 +0200

    fixing responsiveness

--- src/components/communicate.js
+++ src/components/communicate.js
@@ -4,17 +4,16 @@ import Box from './box';
 const Communicate = () => {
   return (<div className='w-5/6 h-screen m-auto'>
  <h1 className="font-poppins font-normal text-5xl ml-3 mb-20 pt-20 uppercase">we can communicate through</h1>
-    <div className='grid grid-cols-3 gap-x-2.5'>
+    <div className='grid grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-3'>
       <Box>
         <div className="m-auto flex flex-col space-y-4">
           <img src="/phone.svg" alt="voice call" className="w-25 m-auto mb-4" />
           <p className="font-poppins font-normal text-5xl text-black text-center mb-4">
             Voice Call
           </p>
-          <p className="font-poppins font-light text-center text-black/80 w-[280px] m-auto">
-            Feeling ready to start a 
-            conversation? Give your therapist a voice
-            call and talk your heart out!
+          <p className="font-poppins font-light text-center text-black/80 w-11/12 m-auto">
+            Feeling ready to start a conversation? Give your therapist a 
+            voice call and talk your heart out!
           </p>
         </div>
       </Box>
@@ -24,7 +23,7 @@ const Communicate = () => {
           <p className="font-poppins font-normal text-5xl text-black text-center mb-4">
             Chat
           </p>
-          <p className="font-poppins font-light text-center text-black/80 w-[220px] m-auto">
+          <p className="font-poppins font-light text-center text-black/80 w-9/12 m-auto">
             Need to talk to someone? 
             Come have a chat with your therapist!
           </p>
@@ -37,7 +36,7 @@ const Communicate = () => {
           <p className="font-poppins font-normal text-5xl text-black text-center mb-4">
             Video Call
           </p>
-          <p className="font-poppins font-light text-center text-black/80 w-[300px] m-auto">
+          <p className="font-poppins font-light text-center text-black/80 w-11/12 m-auto">
             For better experience therapists recommend video calls, but always
             remember that its a choice!
           </p>
--- src/components/tickets.js
+++ src/components/tickets.js
@@ -14,7 +14,7 @@ const Tickets = () => {
     <div className="h-screen uppercase pt-20 w-5/6 mx-auto">
 <h1 className="font-poppins font-normal text-5xl ml-3 mb-2">Purchase tickets</h1>
 <p className="ml-3 text-2xl mb-12">purchase tickets that can be used to book appointments!</p>
-    <div className='grid grid-cols-3 gap-2'>
+    <div className='grid grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-3'>
     <Box>
     <div className="m-auto flex flex-col space-y-4">
       <p className="font-poppins font-normal text-5xl text-black text-center">
@@ -49,7 +49,7 @@ const Tickets = () => {
     </div>
   </Box>
   </div>
-<div className="bg-white border-border-grey shadow-shadow-grey drop-shadow-lg font-poppins font-normal box-border border-2 rounded-[20px] flex flex-col space-y-4 p-8 mt-8 content-center m-auto">
+<div className="bg-white border-border-grey shadow-shadow-grey drop-shadow-lg font-poppins font-normal box-border border-2 rounded-[20px] flex flex-col space-y-4 p-8 mt-8 w-full content-center m-auto">
   <h1 className="font-poppins font-normal text-5xl mb-2 text-center">Are you a counselor?</h1>
   <p className="font-poppins lowercase text-black/80 text-center w-10/12 mx-auto">Interested in joining our mental health platform? You decide your schedule and how much you want to work, we’ll take care of the client referrals and billing details!</p>
   <Button value='learn more' onClick={handleClick()} align='place-self-center'/>
