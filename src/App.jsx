a877e8134c89bb996714f2d6425e0b030f5c3fe7
Nina Hawari <nina.hawari@gmail.com>
2023-01-02 00:20:35 +0200

    refactored navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import Header from './components/Header';
-import RecentBlog from './components/RecentBlog';
-import Communicate from './components/Communicate';
-import Tickets from './components/Tickets';
-import Therapists from './components/Therapists';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Home from './containers/Home';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Therapists />
-      <Communicate />
-      <RecentBlog />
-      <Tickets />
+      <Navbar />
+      <Home />
       <Footer />
     </div>
   );
---
+++ src/containers/Blogs.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Blogs() {
+  return (
+    <div />
+  )
+}
+
+export default Blogs
