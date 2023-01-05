1a36548478a51fd65a42dc6627408764c61aabf6
marshall <mhamad.marshall@gmail.com>
2023-01-05 16:14:40 +0300

    redesigned login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {FcGoogle} from 'react-icons/fc'
 import {AiFillFacebook} from 'react-icons/ai'
 
-import log from '../images/log.png'
+import log from '../assets/log.png'
 
 export default function Login() {
   return (
@@ -13,22 +13,22 @@ export default function Login() {
 
         <div className='flex justify-center items-center h-full'>
         <form className='max-w-[400px] w-full mx-auto bg-white p-8'>
-            <h2 className='text-4xl font-bold text-center py-4 font-Poppins'>Log in</h2>
+            <h2 className='text-4xl font-bold text-center py-4 font-poppins'>LOGIN</h2>
             <div className='flex justify-between py-8'>
-                <p className='border shadow-lg hover:shadow-xl px-6 py-2 relative flex items-center'><AiFillFacebook className='mr-2' /> Facebook</p>
-                <p className='border shadow-lg hover:shadow-xl px-6 py-2 relative flex items-center'><FcGoogle className='mr-2' /> Google</p>
+                <p className='border shadow-lg hover:shadow-xl px-6 py-2 relative flex items-center font-poppins'><AiFillFacebook className='mr-2' /> Facebook</p>
+                <p className='border shadow-lg hover:shadow-xl px-6 py-2 relative flex items-center font-poppins'><FcGoogle className='mr-2' /> Google</p>
             </div>
             <div className='flex flex-col mb-4'>
-                <label>Username</label>
+                <label>Email</label>
                 <input className='border relative bg-gray-100 p-2' type="text" />
             </div>
             <div className='flex flex-col '>
                 <label>Password</label>
                 <input className='border relative bg-gray-100 p-2' type="password" />
             </div>
-            <div className='flex justify-between'>
-               <button className='w-28 h-50 py-4 px-2 mt-8 ml-3 bg-cyan-600 hover:bg-sky-500 scale-x-125 rounded hover:cursor-pointer text-white'>Log In</button>
-               <button className='w-28 h-50 py-4 px-2 mt-8 mr-3 bg-blue-200 hover:bg-sky-300 scale-x-125 rounded hover:cursor-pointer text-cyan-700'>Sign up</button>
+            <div className='flex justify-between font-poppins'>
+               <button className='w-28 h-50 py-4 px-2 mt-8 ml-3 bg-blue-400 hover:bg-blue-200 scale-x-125 rounded hover:cursor-pointer text-black  border-blue-400 font-poppins'>Login</button>
+               <button className='w-28 h-50 py-4 px-2 mt-8 mr-3 hover:bg-blue-200 scale-x-125 rounded hover:cursor-pointer text-blue-400 border border-blue-400 font-poppins'>Signup</button>
             </div>
         </form>
     </div>
