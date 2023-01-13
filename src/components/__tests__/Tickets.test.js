8245efc656318597180f9602a32ce4103947a0e2
Mohammed Diary Mohammed <hamadiary1@gmail.com>
2023-01-14 02:39:41 +0300

    Snapshot tests done

---
+++ src/components/__tests__/Tickets.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Tickets from '../Tickets';
+
+test('Tickets component renders correctly', () => {
+    const component = renderer.create(<Tickets />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+
