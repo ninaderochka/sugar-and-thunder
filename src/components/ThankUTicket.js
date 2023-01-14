3065c09734589d4f97734588c62300a1098febf1
Zeynab Hamad <zaynabhamad950@gmail.com>
2023-01-14 23:01:21 +0300

    Done

--- src/components/ThankUTicket.js
+++ src/components/ThankUTicket.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import Button from './Button';
+
 
 export default function ThankUTicket() {
   return (
@@ -7,11 +9,10 @@ export default function ThankUTicket() {
         <h1 className='text-[50px] tracking-[-1px]' >THANK YOU!</h1>
         <p className='text-[24px] text-contact-us-gray pt-4 pb-12' >You purchase has been submitted, you should receive an email with the receipt soon.</p>
 
-         {/* <Link to="/Home"> */}
+         <Link to="/Home">
          <div className=" text-[24px] pb-64"> 
-        <Button  value="BACK TO HOME" />
-        </div>
-        {/* </Link> */}
+            <Button value="BACK TO HOME" />
+        </div></Link>
         
 
     </div>
