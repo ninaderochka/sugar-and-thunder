2a88565e42efed73175492dee2f7250b1caab305
Zeynab Hamad <zaynabhamad950@gmail.com>
2023-01-16 01:41:23 +0300

    Done with responsive

--- src/components/ThankUSignup.js
+++ src/components/ThankUSignup.js
@@ -4,15 +4,21 @@ import Button from './Button';
 
 export default function ThankUSignup() {
   return (
-    <div className="flex flex-col pt-12 pl-16">
-      <h1 className="text-[50px] tracking-[-1px]">THANK YOU!</h1>
-      <p className="text-[24px] text-contact-us-gray pt-4 pb-10">
-      Your Sign Up request has been received, you will soon receive a confirmation email.<br/>Please follow the steps in the email to complete and activate your account.
+    <div className="flex flex-col pt-16 md:pt-12 lg:pt-12 px-4 md:px-4 lg:px-16 justify-center font-poppins ">
+      <h1 className="text-[30px] md:text-[46] lg:text-[50px] tracking-[-1px]">
+        THANK YOU!
+      </h1>
+      <p className="text-[18px] md:text-[20px] lg:text-[24px] text-contact-us-gray pt-4 pb-10 md:w-[400px] lg:w-full">
+        Your Sign Up request has been received, you will soon receive a
+        confirmation email.
+        <br />
+        Please follow the steps in the email to complete and activate your
+        account.
       </p>
 
       <Link to="/Home">
-        <div className=" text-[24px] pb-64">
-          <Button value="BACK TO HOME" />
+        <div className="text-[16px] md:text-[20px] lg:text-[24px] pb-64 flex justify-center md:justify-start lg:justify-start">
+          <Button className="w-[100px] h-[50px]" value="BACK TO HOME" />
         </div>
       </Link>
     </div>
