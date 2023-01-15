256f57e47d4c1b923ff7a220e16886644deba8c6
Zeynab Hamad <zaynabhamad950@gmail.com>
2023-01-16 02:53:57 +0300

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,12 @@ import Blog from './containers/Blog';
 import About from './containers/About';
 import Contact from './containers/Contact';
 import Signup from './containers/Signup';
-// import EditProfile from './components/EditProfile';
-import Cards from './components/Cards';
-import ThankUTicket from './components/ThankUTicket';
-import AddNewCard from './components/AddNewCard';
-import ThankUSub from './components/ThankUSub';
 import ThankUTherapist from './components/ThankUTherapist';
 
 function App() {
   return (
     <div className="App">
       <ThankUTherapist />
-      <ThankUSub />
-      <ThankUTicket />
-      <AddNewCard />
-      <Cards />
-      {/* <EditProfile/> */}
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
