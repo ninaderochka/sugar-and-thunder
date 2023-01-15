971cbb690db4ed3b074d1d3f794dc0450aa32677
Mohammed Diary Mohammed <hamadiary1@gmail.com>
2023-01-15 23:56:25 +0300

    Done

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -56,7 +56,7 @@ function Tickets() {
           schedule and how much you want to work, we’ll take care of the client
           referrals and billing details!
         </p>
-        <Link to ='./Requirements'><Button value="learn more" align="place-self-center" /></Link>
+        <Link className="self-center" to ='./Requirements'><Button value="learn more" align="place-self-center" /></Link>
       </div>
     </div>
   );
