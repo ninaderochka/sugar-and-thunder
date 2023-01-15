4cab9d1561f5ad3229369c8c1568de36ad359e8f
Mohammed Diary Mohammed <hamadiary1@gmail.com>
2023-01-15 17:22:22 +0300

    new tests

---
+++ src/components/__tests__/AboutTeam.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import AboutTeam from '../AboutTeam';
+
+test('renders correctly', () => {
+    const { asFragment } = render(<AboutTeam />);
+    expect(asFragment()).toMatchSnapshot();
+});
