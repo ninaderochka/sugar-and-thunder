fa7459151452d76097b08fdaaa37fe4e13ff3f0a
Zeynab Hamad <zaynabhamad950@gmail.com>
2023-01-20 18:20:37 +0300

    .

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -5,7 +5,7 @@ import Button from './Button';
 
 export default function EditProfile() {
   return (
-    <div className="flex pl-24 pt-12 font-poppins">
+    <div className="flex md:pl-2 md:pt-4 lg:pl-24 lg:pt-12 font-poppins">
       <div className="">
         <div className="relative flex justify-center">
           <div className="absolute -bottom-2 lg:-bottom-4 bg-white z-0 rounded-full border-2 border-black  w-12 h-12 md:w-16 md:h-16 flex justify-center items-center">
@@ -16,14 +16,15 @@ export default function EditProfile() {
       </div>
 
       {/* first part */}
-      <div className=" text-[24px] ml-24 space-y-2">
+      <div className="text-[12px] md:text-[18px] lg:text-[24px] ml-24 space-y-2">
         <h1 className="text-[50px] font-medium mb-8 pt-12">PROFILE INFO</h1>
 
         <div className="flex justify-between space-x-12">
           <p>Full Name</p>
           <input
-            className="border rounded-[10px] border-black/0.12 h-[68px] w-[475px]"
+            className="border rounded-[10px] border-black/0.12 h-[68px] w-[475px] px-4"
             type="text"
+            placeholder='Enter your name'
             maxLength={50}
           />
         </div>
@@ -40,19 +41,20 @@ export default function EditProfile() {
         <div className="flex justify-between space-x-12">
           <p>Hobbies</p>
           <input
-            className="border rounded-[10px] border-black/0.12 h-[68px] w-[475px]"
+            className="border rounded-[10px] border-black/0.12 h-[68px] w-[475px] px-4"
             type="text"
             maxLength={50}
           />
         </div>
 
-        <div className="flex space-x-[105px]">
-          <p>Family Size</p>
+        <div className="flex">
+          <p className=''>Family Size</p>
           <input
-            className="border rounded-[10px] border-black/0.12 h-[68px] w-[68px]"
-            type="number"
+            className="border rounded-[10px] border-black/0.12 h-[68px] w-[68px] ml-[206px] px-1"
+            type="text"
             maxLength={5}
           />
+          <p className='ml-5 py-2'>Member(s)</p>
         </div>
 
         <div className="flex justify-between space-x-12">
@@ -66,16 +68,17 @@ export default function EditProfile() {
         <div className="flex justify-between space-x-12">
           <p>Birth Date</p>
           <input
-            className="border rounded-[10px] border-black/0.12 h-[68px] w-[475px]"
-            type="date"
-            placeholder="MM / YY"
+            className="border rounded-[10px] border-black/0.12 h-[68px] w-[475px] px-4"
+          
+            placeholder="MM/ DD/ YY"
           />
         </div>
 
         <div className="flex justify-between space-x-12">
           <p>Email</p>
           <input
-            className="border rounded-[10px] border-black/0.12 h-[68px] w-[475px]"
+            className="border rounded-[10px] border-black/0.12 h-[68px] w-[475px] px-4"
+            placeholder='name@gmail.com'
             type="email"
           />
         </div>
@@ -83,7 +86,7 @@ export default function EditProfile() {
         <div className="flex justify-between space-x-12">
           <p>Phone Number</p>
           <input
-            className="border rounded-[10px] border-black/0.12 h-[68px] w-[475px]"
+            className="border rounded-[10px] border-black/0.12 h-[68px] w-[475px] px-4"
             type="tel"
             maxLength={15}
           />
@@ -92,37 +95,38 @@ export default function EditProfile() {
         <div className="flex justify-between space-x-12">
           <p>Upload ID</p>
           <input
-            className="border rounded-[10px] border-black/0.12 h-[68px] w-[475px]"
+            className="border rounded-[10px] border-black/0.12 h-[68px] w-[475px] px-4"
             type="file"
+            placeholder='choose file'
           />
         </div>
 
         {/* Sencond part */}
 
-        <div className=" text-[24px]">
+        <div className=" text-[24px] space-y-2">
           <h1 className="text-[50px] mb-8 pt-12 md:pt-0">Security</h1>
 
           <div className="flex justify-between space-x-12">
             <p>Password</p>
             <input
-              className="border rounded-[10px] border-black/0.12 h-[68px] w-[475px]"
-              type="email"
+              className="border rounded-[10px] border-black/0.12 h-[68px] w-[475px] px-4"
+              type="password"
             />
           </div>
 
           <div className="flex justify-between space-x-12">
             <p>Confirm Password</p>
             <input
-              className="border rounded-[10px] border-black/0.12 h-[68px] w-[475px]"
-              type="tel"
+              className="border rounded-[10px] border-black/0.12 h-[68px] w-[475px] px-4"
+              type="password"
               maxLength={15}
             />
           </div>
 
-          <div>
-            <Button value="save Changes" />
-            <Button value="DELETE ACCOUNT" />
-            <Button value="cancel" />
+          <div className='flex justify-between text-[12px] '>
+            <Button className="w-1/4" value="save Changes" />
+            <Button className="w-1/4" value="DELETE ACCOUNT" />
+            <Button className="w-1/4" value="cancel" />
           </div>
         </div>
       </div>
