e24f1db9a6d824482983f7b4e8bede85f7fd34c1
marshall <mhamad.marshall@gmail.com>
2023-01-21 16:04:04 +0300

    Fixed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import { store } from './app/store';
 import App from './App';
 import './index.css';
-
+import { UserAuthContextProvider } from './Components/Authentication';
+import { BrowserRouter } from 'react-router-dom';
 const container = document.getElementById('root');
 const root = createRoot(container);
-
 root.render(
-  <React.StrictMode>
+  /*  <React.StrictMode> */
+  <UserAuthContextProvider>
+    {/* <Provider store={store}> */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+    {/* </Provider> */}
+  </UserAuthContextProvider>
+  /* </React.StrictMode> */
 );
