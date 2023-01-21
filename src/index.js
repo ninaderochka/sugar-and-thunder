506bb29c7b0afa291c96308882739195181c535a
Nina Hawari <nina.hawari@gmail.com>
2023-01-21 13:42:29 +0200

    fixes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import './i18n.js';
+import './i18n';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
